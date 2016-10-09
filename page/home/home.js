66c9ecf071a9e2cf1f2c1c2ec060f5aa58e1ac2f
gakaki <gakaki@gmail.com>
2016-10-09 13:56:13 +0800

    add all

--- page/home/home.js
+++ page/home/home.js
@@ -3,33 +3,41 @@ var Api     = require('../../utils/api.js');
 var util    = require('../../utils/util.js');
 
 Page({
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  tapName(e) {
+    console.log(e)
+  },
+  lower(e){
+    alert(e)
 
+  },
   data: {
-    data:[]
-
+    data:[],
+    hidden:true
   },
   myFun(){
     console.log('vue react')
   },
   onLoad: function () {
-
+    this.setData({
+      hidden:false
+    });
     console.log(this.myFun())
 
     Api.home( res => {
       console.log(res)
       //更新数据
       this.setData({
-          data:res.modules
+          data:res.modules,
+          hidden:true
       });
     });
 
   },
-
-
-  bindchange(e){
-
-
-  }
 
 
 })
